e2ca66fc6127f9401425557aa11132a9a64343a8
Iruyan_Zak <yuri.kazuna@gmail.com>
2018-07-10 02:51:23 +0900

    Multiline CSV parse and simple deck storing.

--- src/LocalStorage.elm
+++ src/LocalStorage.elm
@@ -1,13 +1,15 @@
 port module LocalStorage exposing (..)
-
+import Dict exposing (Dict)
 
 type alias Record =
     { id : String
     , name : String
     }
 
+type alias DeckSource =
+    List (List (String, String))
 
 port getDeckReq : String -> Cmd msg
-port setDeckReq : (String, List Record) -> Cmd msg
-port getDeckRes : (Maybe (List Record) -> msg) -> Sub msg
+port setDeckReq : (String, DeckSource) -> Cmd msg
+port getDeckRes : (Maybe DeckSource -> msg) -> Sub msg
 port setDeckRes : (() -> msg) -> Sub msg
--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,14 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, table, tr, td, p, text, h1)
-import Html
-import Html.Attributes exposing (class)
-import Html.Attributes as Attr
-import Html.Events as Events
-import CsvDecode exposing ((|=))
-import CsvDecode
-import LocalStorage as LS
+import Dict exposing (Dict)
 import FileReader exposing (NativeFile)
 import FileReader.FileDrop as DZ
+import Html
+import Html exposing (Html, div, table, tr, td, h1, p, text)
+import Html.Attributes as Attr
+import Html.Attributes exposing (class)
+import Html.Events as Events
+import LocalStorage as LS
 import Task
 
 ---- MODEL ----
@@ -20,14 +19,21 @@ type alias Record =
     , name : String
     }
 
+type alias DeckSource =
+    List (List (String, String))
+
+type alias Deck =
+    List (Dict String String)
+
 type alias Model =
-    { deck : Result String (List Record)
+    { deck : Deck
+    , message : String
     , dragHovering : Int
     }
 
 
 init : ( Model, Cmd Msg )
-init = Model (Err "Not loaded.") 0 ! [LS.getDeckReq "DEV-1"]
+init = Model [] "Deck is not loaded." 0 ! [LS.getDeckReq "DEV-1"]
 
 
 ---- UPDATE ----
@@ -39,7 +45,7 @@ type Msg
     | OnDrop (List NativeFile)
     | FileContentsGot (Result FileReader.Error String)
     | ItemSet ()
-    | ItemGet (Maybe (List Record))
+    | ItemGet (Maybe DeckSource)
 
 
 subscriptions : Model -> Sub Msg
@@ -62,24 +68,25 @@ update msg model =
         ItemGet valueMaybe ->
             case valueMaybe of
                 Nothing ->
-                    {model | deck = Err "getItem failed."} ! []
+                    {model | message = "getItem failed."} ! []
 
-                Just records ->
-                    {model | deck = Ok records} ! []
+                Just deckSource ->
+                    let
+                        _ = Debug.log (toString deckSource) ()
+                    in
+
+                    {model
+                        | deck = List.map Dict.fromList deckSource
+                        , message = "Deck is loaded."} ! []
 
         OnDrop [file] ->
             model ! [getFileContents file]
 
         FileContentsGot (Ok csv) ->
             let
-                deck = buildFromCSV csv
+                deckSource = parseCSV csv
             in
-                {model | deck = deck} ! case deck of
-                    Ok deck ->
-                        [LS.setDeckReq ("DEV-1", deck)]
-
-                    Err _ ->
-                        []
+                {model | deck = List.map Dict.fromList deckSource} ! [LS.setDeckReq ("DEV-1", deckSource)]
 
         _ -> model ! []
 
@@ -106,12 +113,8 @@ view model =
                     , Attr.multiple False
                     ] []
                 ]
-            , case model.deck of
-                Ok records ->
-                    deckTable records
-
-                Err msg ->
-                    p [] [text msg]
+            , p [] [ text model.message]
+            , deckTable model.deck
             ]
 
 
@@ -126,16 +129,15 @@ dropZoneClass_ dragHovering =
             class "drop-zone" :: dzAttrs
 
 
-deckTable : List Record -> Html Msg
-deckTable records =
+deckTable : Deck -> Html Msg
+deckTable deck =
     let
         cells =
-            records
-            |> List.map (\{ id, name } ->
+            List.map (\dict ->
                 tr []
-                    [ td [] [ text id ]
-                    , td [] [ text name ]
-                    ])
+                    [ td [] [  Dict.get "ID"   dict |>  Maybe.withDefault "" |> text ]
+                    , td [] [  Dict.get "card_name" dict |>  Maybe.withDefault "" |> text ]
+                    ]) deck
     in
         table [] cells
 
@@ -153,12 +155,70 @@ main =
         }
 
 
-buildFromCSV : String -> Result String (List Record)
-buildFromCSV csv =
+parseCSV : String -> DeckSource
+parseCSV csvStr =
     let
-        decoder =
-            CsvDecode.succeed Record
-                |= CsvDecode.field "ID"
-                |= CsvDecode.field "name"
+        csv = String.toList csvStr
+
+        (rawHeader, _, rawBody) =
+            split (\c -> c == '\n') csv
+
+        header =
+            String.fromList rawHeader
+            |> String.split ","
+            |> List.map String.trim
+
+        parse body =
+            case parseCSVRecord body of
+                (head, []) ->
+                    [head]
+
+                (head, tail) ->
+                    head :: parse tail
     in
-        CsvDecode.run decoder csv
+        parse rawBody
+        |> List.map (String.fromList >> String.split "," >> List.map String.trim >> zip header)
+
+
+parseCSVRecord : List Char -> (List Char, List Char)
+parseCSVRecord unparsed =
+    let
+        (parsed, pivot, rest) =
+            split (\c -> c == '\n' || c == '"') unparsed
+    in
+        case pivot of
+            Nothing ->
+                (parsed, [])
+
+            Just c ->
+                if c == '\n' then
+                    (parsed, rest)
+                else
+                    let
+                        (quoted, _, rest_) =
+                            split (\c -> c == '"') rest
+                        (following, rest__) =
+                            parseCSVRecord rest_
+                    in
+                        (parsed ++ quoted ++ following, rest__)
+
+
+split : (a -> Bool) -> List a -> (List a, Maybe a, List a)
+split pred seq =
+    let
+        split_ buf rest =
+            case rest of
+                [] ->
+                    (List.reverse buf, Nothing, [])
+                x :: xs ->
+                    if pred x then
+                        (List.reverse buf, Just x, xs)
+                    else
+                        split_ (x :: buf) xs
+    in
+        split_ [] seq
+
+
+zip : List a -> List b -> List (a, b)
+zip seq1 seq2 =
+    List.map2 (,) seq1 seq2
